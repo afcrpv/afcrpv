23bd0967e672c75d6c326087004f606d036f7a31
Andrea D'Amico <andrea.damico@gmail.com>
2012-06-22 10:32:16 +0200

    Created application using Refinery CMS 2.0.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afcrpv::Application.config.secret_token = '339412d7f749b16ea93b3946b4ae41b94d723dc83ec1de14f7b2181f0b0ccde46a8373a2ccea32b7bc3f2f0ce8b1e53a3fb9519df6e9c39e9ae6024920843214'
