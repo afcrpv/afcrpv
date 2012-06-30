2acfaca5143243338eb56c6a04408e6b323d99d6
Andrea D'Amico <andrea.damico@gmail.com>
2012-06-30 22:00:38 +0200

    custom header, using navbar from twitter bs for menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def custom_menu_branch_css(local_assigns)
+    options = local_assigns.dup
+    options.update(:sibling_count => options[:menu_branch].shown_siblings.length) unless options[:sibling_count]
+
+    css = []
+    css << 'active' if selected_page_or_descendant_page_selected?(local_assigns[:menu_branch])
+    css << 'dropdown' if (local_assigns[:menu_branch].children unless local_assigns[:hide_chilren]).any?
+    css
+  end
 end
