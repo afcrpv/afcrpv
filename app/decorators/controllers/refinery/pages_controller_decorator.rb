c3da5bd41621bc061e2d275fe29cc8672f4ba09a
Andrea D'Amico <andrea.damico@gmail.com>
2012-09-07 17:01:42 +0200

    getting rss feeds from agencies and publishing to home page

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 
 require 'simple-rss'
+require 'open-uri'
 
 Refinery::PagesController.class_eval do
   before_filter :find_pv_news, only: [:home]
@@ -9,14 +10,18 @@ Refinery::PagesController.class_eval do
   protected
 
   def find_agences
-    @agences = [
-      {name: "HAS", description: "dernières synthèses d'avis"},
-      {name: "ANSM", description: "dernières informations de sécurité"},
-      {name: "FDA", description: "medwatch safety alerts"}
+    @agences ||= [
+      {name: "HAS", description: "dernières synthèses d'avis", news: get_rss_entries('http://www.has-sante.fr/portail/feed/Rss2.jsp?id=c_754989')},
+      {name: "ANSM", description: "dernières informations de sécurité", news: get_rss_entries('http://ansm.sante.fr/syndication/rss')},
+      {name: "FDA", description: "medwatch safety alerts", news: get_rss_entries('http://www.fda.gov/AboutFDA/ContactFDA/StayInformed/RSSFeeds/MedWatch/rss.xml')}
 ]
   end
 
   def find_pv_news
     @pv_news = []
+  end
+
+  def get_rss_entries(url, limit=5)
+    SimpleRSS.parse(open(url)).entries[1..limit]
   end
 end
