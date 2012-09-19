0c07ddd40f82e2ba44e75bc07542ebae2874cc6f
Andrea D'Amico <andrea.damico@gmail.com>
2012-09-19 10:20:04 +0200

    don't limit news results

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -11,6 +11,6 @@ Refinery::PagesController.class_eval do
   end
 
   def find_pv_news
-    @pv_news = (Refinery::Events::Event.limit(5) + Refinery::Emplois::Emploi.limit(5)).sort {|a, b| b.created_at <=> a.created_at}
+    @pv_news = (Refinery::Events::Event.scoped + Refinery::Emplois::Emploi.scoped).flatten.sort {|a, b| b.created_at <=> a.created_at}
   end
 end
