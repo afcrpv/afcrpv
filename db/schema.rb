e7c25d8ce8b77504914471d99b40f5747c1acea3
Andrea D'Amico <andrea.damico@gmail.com>
2012-11-16 13:11:37 +0100

    changes to schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,54 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918083923) do
-
-  create_table "refinery_agences", :force => true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.string   "feed_url"
-    t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "slug"
-    t.boolean  "show_on_home"
-  end
-
-  create_table "refinery_categories", :force => true do |t|
-    t.string   "title"
-    t.integer  "position"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "description"
-  end
-
-  create_table "refinery_emplois", :force => true do |t|
-    t.string   "organisation"
-    t.string   "name"
-    t.string   "email"
-    t.string   "telephone"
-    t.text     "address"
-    t.string   "title"
-    t.text     "description"
-    t.date     "expires_on"
-    t.integer  "attachment_id"
-    t.integer  "position"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "slug"
-  end
-
-  create_table "refinery_events", :force => true do |t|
-    t.string   "title"
-    t.integer  "category_id"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.text     "description"
-    t.integer  "position"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "slug"
-  end
+ActiveRecord::Schema.define(:version => 20120914115710) do
 
   create_table "refinery_images", :force => true do |t|
     t.string   "image_mime_type"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,5 @@
 # Added by Refinery CMS Pages extension
 #Refinery::Pages::Engine.load_seed
 
-# Added by Refinery CMS Agences extension
-Refinery::Agences::Engine.load_seed
-
-# Added by Refinery CMS Events extension
-Refinery::Events::Engine.load_seed
-
 # Added by Refinery CMS Inquiries engine
 Refinery::Inquiries::Engine.load_seed
-
-# Added by Refinery CMS Emplois extension
-Refinery::Emplois::Engine.load_seed
