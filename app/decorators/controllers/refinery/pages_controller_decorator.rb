7fa094aa66d29dba07d14a5eec54cd797f03ed5b
Andrea D'Amico <andrea.damico@gmail.com>
2012-11-16 13:25:38 +0100

    cleaning home page and page controller decorator

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,16 +1,7 @@
 # encoding: UTF-8
 
 Refinery::PagesController.class_eval do
-  before_filter :find_pv_news#, only: [:home, :actualites]
-  before_filter :find_agences, only: [:home]
 
   protected
 
-  def find_agences
-    @agences = Refinery::Agences::Agence.showable_on_home
-  end
-
-  def find_pv_news
-    @pv_news = (Refinery::Events::Event.scoped + Refinery::Emplois::Emploi.scoped).flatten.sort {|a, b| b.created_at <=> a.created_at}
-  end
 end
