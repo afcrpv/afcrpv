318d5fa3c6b95f87c08585c3a488e8675c95a6e6
Andrea D'Amico <andrea.damico@gmail.com>
2013-02-13 15:12:52 +0100

    adding edit/update action and action link

--- vendor/extensions/documents/app/controllers/refinery/documents/documents_controller.rb
+++ vendor/extensions/documents/app/controllers/refinery/documents/documents_controller.rb
@@ -5,6 +5,8 @@ module Refinery
 
       before_filter :find_all_categories, only: [:new, :edit, :create, :update]
       before_filter :find_page
+      before_filter :edit_document_or_redirect, only: [:new, :edit]
+      helper_method :authorised_documents_user?
 
       def index
         # you can use meta fields from your model instead (e.g. browser_title)
@@ -22,20 +24,28 @@ module Refinery
       end
 
       def new
-        if current_refinery_user.has_role?('documents') or current_refinery_user.is_admin?
-          @document = ::Refinery::Documents::Document.new
-        else
-          redirect_to refinery.documents_documents_path, notice: "Vous n'êtes pas autorisé à voir cette page !"
-        end
+      end
+
+      def edit
       end
 
       def create
         resource = Resource.create(file: params[:document][:fichier])
         @document = ::Refinery::Documents::Document.new(params[:document].merge({fichier: resource}))
         if @document.save
-          redirect_to refinery.documents_documents_path
+          redirect_to refinery.documents_documents_path, success: "Le document a été créé avec succès."
         else
           render :new
+        end
+      end
+
+      def update
+        @document = ::Refinery::Documents::Document.find(params[:id])
+
+        if @document.update_attributes(params[:document])
+          redirect_to refinery.documents_documents_path, success: "Le document a été modifié."
+        else
+          render :edit
         end
       end
 
@@ -47,6 +57,24 @@ module Refinery
 
       def find_page
         @page = ::Refinery::Page.where(:link_url => "/documents").first
+      end
+
+      def authorised_documents_user?
+        current_refinery_user && (current_refinery_user.has_role?('documents') or current_refinery_user.is_admin?)
+      end
+
+      def edit_document_or_redirect
+        if authorised_documents_user?
+          method = :new
+          args = []
+          if params[:id].present?
+            method = :find
+            args = params[:id]
+          end
+          @document = ::Refinery::Documents::Document.send(method, args)
+        else
+          redirect_to refinery.documents_documents_path, notice: "Vous n'êtes pas autorisé à voir cette page !"
+        end
       end
 
     end
