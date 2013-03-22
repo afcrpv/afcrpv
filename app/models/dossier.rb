ee517407c0a394c880cd1e64f6108ee000857303
Andrea D'Amico <andrea.damico@gmail.com>
2013-03-22 16:18:35 +0100

    cleanup dossier/enquete models for normal association

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -1,20 +1,24 @@
 #encoding: utf-8
 class Dossier < ActiveRecord::Base
-  include HydraAttribute::ActiveRecord
-
-  attr_accessible :code_bnpv, :date_recueil, :doublon, :j_evenement, :m_evenement, :a_evenement, :comm_evenement, :gravite, :evolution, :commentaire, :position, :patient_attributes, :hydra_set_id, :evenement_id, :refinery_crpv_id, :traitements_attributes
+  attr_accessible :code_bnpv, :date_recueil, :doublon, :j_evenement, :m_evenement, :a_evenement, :comm_evenement, :gravite, :evolution, :commentaire, :patient_attributes, :enquete_id, :evenement_id, :refinery_crpv_id, :traitements_attributes
 
   validates :code_bnpv, presence: true, uniqueness: true
 
   has_one :patient
   belongs_to :evenement
-  belongs_to :enquete, foreign_key: :hydra_set_id
+  belongs_to :enquete
   belongs_to :crpv, class_name: "::Refinery::Crpvs::Crpv", foreign_key: :refinery_crpv_id
   has_many :traitements, dependent: :destroy
   has_many :medicaments, through: :traitements
 
   accepts_nested_attributes_for :patient, reject_if: :all_blank
   accepts_nested_attributes_for :traitements, reject_if: :all_blank, allow_destroy: true
+
+  ONI = [
+    ["Oui", 1],
+    ["Non", 2],
+    ["NSP", 3]
+  ]
 
   GRAVITE = [
     "décès",
--- app/models/enquete.rb
+++ app/models/enquete.rb
@@ -1,26 +1,23 @@
 class Enquete < ActiveRecord::Base
-  attr_accessible :name, :evenement_list, :medicament_list, :description, :fields_attributes
-  #has_many :dossiers
-  #has_many :evenement_choices
-  #has_many :evenements, through: :evenement_choices, dependent: :destroy, order: "LOWER(name)"
-  #has_many :medicament_choices
-  #has_many :medicaments, through: :medicament_choices, dependent: :destroy, order: "LOWER(name)"
-
-  has_many :fields, class_name: "EnqueteField", order: :position
-  accepts_nested_attributes_for :fields, allow_destroy: true
+  attr_accessible :name, :description, :evenement_list, :medicament_list, :meta_type_properties_attributes
+  has_many :dossiers
+  has_many :evenement_choices
+  has_many :evenements, through: :evenement_choices, dependent: :destroy, order: "LOWER(name)"
+  has_many :medicament_choices
+  has_many :medicaments, through: :medicament_choices, dependent: :destroy, order: "LOWER(name)"
 
   def to_s
     description
   end
 
-  #%w[evenement medicament].each do |method|
-    #define_method :"#{method}_list" do
-      #self.send(:"#{method}s").map(&:name).join(", ")
-    #end
+  %w[evenement medicament].each do |method|
+    define_method :"#{method}_list" do
+      self.send(:"#{method}s").map(&:name).join(", ")
+    end
 
-    #define_method :"#{method}_list=" do |names|
-      #self.send(:"#{method}s=",
-                #names.split(",").map {|name| method.classify.constantize.where(name: name.strip).first_or_create!})
-    #end
-  #end
+    define_method :"#{method}_list=" do |names|
+      self.send(:"#{method}s=",
+                names.split(",").map {|name| method.classify.constantize.where(name: name.strip).first_or_create!})
+    end
+  end
 end
