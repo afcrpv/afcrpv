90f136d24723e0f5de5964199b678aa591d1aba6
Andrea D'Amico <andrea.damico@gmail.com>
2013-06-28 14:44:19 +0200

    secure secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Afcrpv::Application.config.secret_token = '60f6dd5b39cdc55fcddc6d5f3462d58d60e8c10f820b0f6bf590f63723b280acf5c32c7d4365136d176dbf2c4895c5aa15d132e3fa00911fedf2c30304b81cf3'
+def find_secure_token
+  Rails.env.development? ? '60f6dd5b39cdc55fcddc6d5f3462d58d60e8c10f820b0f6bf590f63723b280acf5c32c7d4365136d176dbf2c4895c5aa15d132e3fa00911fedf2c30304b81cf3' : ENV['APP_SECRET_TOKEN']
+end
+
+Afcrpv::Application.config.secret_token = find_secure_token
